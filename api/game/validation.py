975ed7afb823f02b76947249b0876cd32dccfa7b
GoEdgar <edgar.gorobchuk.py@gmail.com>
2023-03-28 15:46:44 +0300

    Change validation constraints

--- api/game/validation.py
+++ api/game/validation.py
@@ -33,11 +33,11 @@ class GameBase(Validator):
 
     # [[player_id, foul_count], ...]
     players: conlist(conlist(int, min_items=2, max_items=2),
-                     min_items=2, max_items=2) = None
+                     min_items=10, max_items=10) = None
 
     # [[player_id, additional_points], ...]
     best_players: conlist(conlist(float, min_items=2, max_items=2),
-                          min_items=2, max_items=2) = None
+                          min_items=0, max_items=10) = None
 
 
 class GameCreateRequest(GameBase):
